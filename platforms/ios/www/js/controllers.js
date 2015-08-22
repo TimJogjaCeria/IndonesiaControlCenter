e979cd1d2f5a632a75e9bf98bfa95d1d5dfb0334
Eko Wibowo <swdev.bali@gmail.com>
2015-08-22 20:07:38 +0700

    okay, bisa menampilkan data dari JSON luar

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -1,37 +1,36 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $http) {
-            $http.get('http://128.199.185.100/kawalberas/indikator-vital.json').then(function(resp) {
-                                                                                     console.log('Success', resp);
-                                                                                     $scope.tasks = responses;
-                                                                                     // For JSON responses, resp.data contains the result
-                                                                                     }, function(err) {
-                                                                                     console.error('ERR', err);
-                                                                                     // err.status will contain the status code
-                                                                                     })
+            console.log('Success!');
+            console.log('OOkay!');
+            $http.get('http://128.199.185.100/kawalberas/indikator-vital.json').then(function(responses){
+                  $scope.indikators = responses.data;
+                                                                                     console.log(responses);
+                                                                                     
+                })
             })
 
 .controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
+            
+            $scope.chats = Chats.all();
+            $scope.remove = function(chat) {
+            Chats.remove(chat);
+            };
+            })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+            $scope.chat = Chats.get($stateParams.chatId);
+            })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+            $scope.settings = {
+            enableFriends: true
+            };
+            });
